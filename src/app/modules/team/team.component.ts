2e687902243d54fc815fff9d772fb8a9843e9374
Khingdave <dheyved1@gmail.com>
2021-10-27 21:24:00 +0100

    Team and Speakers component

--- src/app/modules/team/team.component.ts
+++ src/app/modules/team/team.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
+  teamMembers = [
+    { id: 1, image: '../../../assets/img/home.7c51634b.jpg', name: "Name", role: "Role", twitter: "Twitter", linkedin: "LinkedIn" },
+    { id: 2, image: '../../../assets/img/home.7c51634b.jpg', name: "Name", role: "Role", twitter: "Twitter", linkedin: "LinkedIn" },
+    { id: 3, image: '../../../assets/img/home.7c51634b.jpg', name: "Name", role: "Role", twitter: "Twitter", linkedin: "LinkedIn" },
+    { id: 4, image: '../../../assets/img/home.7c51634b.jpg', name: "Name", role: "Role", twitter: "Twitter", linkedin: "LinkedIn" },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
