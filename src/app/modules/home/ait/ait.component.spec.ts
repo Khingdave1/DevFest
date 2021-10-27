96bca60319d5e0b265f4abfad06105935509e2cd
Khingdave <dheyved1@gmail.com>
2021-10-27 14:23:30 +0100

    venue components

---
+++ src/app/modules/home/ait/ait.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AitComponent } from './ait.component';
+
+describe('AitComponent', () => {
+  let component: AitComponent;
+  let fixture: ComponentFixture<AitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AitComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
