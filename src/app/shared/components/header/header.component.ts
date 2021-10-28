ce7ba5315392e8c4c150e5653bc058cc30311f5b
Khingdave <dheyved1@gmail.com>
2021-10-28 15:44:42 +0100

    Worked on mobile Nav

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  hamClick: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Toggle Menu
+  openMenu() {
+    this.hamClick = !this.hamClick
   }
 
 }
