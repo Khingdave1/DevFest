64972c5aad864c4fb8b52656ee879c720083ffc0
Khingdave <dheyved1@gmail.com>
2021-11-17 14:11:19 +0100

    updated faq

--- src/app/modules/faq/faq.component.ts
+++ src/app/modules/faq/faq.component.ts
@@ -8,14 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class FaqComponent implements OnInit {
 
   faqQuest: any;
-  yes: any;
 
-  faqs = [
-    { id: 1, question: "What is DevFest?", answer: "DevFests are community led developer events hosted by Google Developers Groups around the globe." },
-    { id: 2, question: "What are GDGs?", answer: "GDGs are focused on community building and learning about Google’s technologies." },
-    { id: 3, question: "Who can register for DevFest?", answer: "Beginners, experts, literally everyone. Anyone with the passion of learning, who’s fond of technology, passionate programmers , everybody can register and attend." },
-    { id: 4, question: "What will attendees get by participating in the event?", answer: "We have a plethora of AMAZING perks awaiting for you!" },
-  ]
+  data: any =
+    [
+      {
+        "parentName": "What is DevFest?",
+        "childProperties":
+          [
+            { "propertyName": "DevFests are community led developer events hosted by Google Developers Groups around the globe." }
+          ]
+      },
+      {
+        "parentName": "What are GDGs?",
+        "childProperties":
+          [
+            { "propertyName": "GDGs are focused on community building and learning about Google’s technologies" }
+          ]
+      },
+      {
+        "parentName": "Who can register for DevFest?",
+        "childProperties":
+          [
+            { "propertyName": "Beginners, experts, literally everyone. Anyone with the passion of learning, who’s fond of technology, passionate programmers , everybody can register and attend." }
+          ]
+      },
+      {
+        "parentName": "What will attendees get by participating in the event?",
+        "childProperties":
+          [
+            { "propertyName": "We have a plethora of AMAZING perks awaiting for you!" }
+          ]
+      }
+    ]
 
   constructor() { }
 
@@ -25,16 +49,36 @@ export class FaqComponent implements OnInit {
     // Array.prototype.forEach.call(this.yes, i => {
 
     // });
+
+
   }
 
-  showMore(event: any) {
-    // this.yes = event.currentTarget.children[1]
-    // this.yes.classList.toggle('show')
+  // showMore(event: any) {
+  //   // this.yes = event.currentTarget.children[1]
+  //   // this.yes.classList.toggle('show')
 
-    this.faqQuest = event.currentTarget.parentElement.children[1]
-    this.faqQuest.classList.toggle('show')
+  //   this.faqQuest = event.currentTarget.parentElement.children[1]
+  //   this.faqQuest.classList.toggle('show')
 
-    console.log(this.faqQuest)
+  //   console.log(this.faqQuest)
+  // }
+
+  toggleAccordian(event: any, index: any) {
+    // var element = event.target;
+    // element.classList.toggle("active");
+    var panel = event.currentTarget.parentElement.children[1];
+    panel.classList.toggle("show");
+    // if (this.data[index].isActive) {
+    //   this.data[index].isActive = false;
+    // } else {
+    //   this.data[index].isActive = true;
+    // }
+    // var panel = element.nextElementSibling;
+    // if (panel.style.maxHeight) {
+    //   panel.style.maxHeight = null;
+    // } else {
+    //   panel.classList.toggle("show");
+    // }
   }
 
 }
