38261e2cb20caef9a68ff7c97e326c961c3bd2f3
Khingdave <dheyved1@gmail.com>
2021-11-22 22:33:29 +0100

    Added Wale's image

--- src/app/modules/team/team.component.ts
+++ src/app/modules/team/team.component.ts
@@ -12,7 +12,7 @@ export class TeamComponent implements OnInit {
     { id: 2, image: '../../../assets/img/team/blessing_eloho_awhensebe.jpg', name: "Blessing Eloho Awhensebe", role: "WTM Ambassador", twitter: "Twitter", linkedin: "LinkedIn" },
     { id: 3, image: '../../../assets/img/home.7c51634b.jpg', name: "Enakhe Theophilus", role: "Assistant Organizer", twitter: "Twitter", linkedin: "LinkedIn" },
     { id: 4, image: '../../../assets/img/team/aremu_david.jpeg', name: "Aremu David", role: "Chapter Mentor", twitter: "https://mobile.twitter.com/Khingdave2", linkedin: "https://www.linkedin.com/in/aremu-david-64a63a193/" },
-    { id: 5, image: '../../../assets/img/home.7c51634b.jpg', name: "Ajayi Olawale", role: "Chapter Mentor", twitter: "Twitter", linkedin: "LinkedIn" },
+    { id: 5, image: '../../../assets/img/team/ajayi_olawale.jpeg', name: "Ajayi Olawale", role: "Chapter Mentor", twitter: "Twitter", linkedin: "LinkedIn" },
     { id: 6, image: '../../../assets/img/home.7c51634b.jpg', name: "Idibia Isaac", role: "Chapter Mentor", twitter: "Twitter", linkedin: "LinkedIn" },
   ]
 
