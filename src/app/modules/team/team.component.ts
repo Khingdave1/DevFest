866a8537f36cd8fb2dd8e1a79c40688f6c75e67b
Khingdave <dheyved1@gmail.com>
2021-11-25 11:35:46 +0100

    Updated footer

--- src/app/modules/team/team.component.ts
+++ src/app/modules/team/team.component.ts
@@ -10,7 +10,7 @@ export class TeamComponent implements OnInit {
   teamMembers = [
     { id: 0, image: '../../../assets/img/team/ayodeji_ogundare.jpg', name: "Ayodeji Ogundare", role: "GDG Organizer", twitter: "https://twitter.com/dejisol", linkedin: "https://www.linkedin.com/in/dejisol" },
     { id: 1, image: '../../../assets/img/team/blessing_eloho_awhensebe.jpg', name: "Blessing Eloho Awhensebe", role: "WTM Ambassador", twitter: "Twitter", linkedin: "LinkedIn" },
-    { id: 2, image: '../../../assets/img/home.7c51634b.jpg', name: "Enakhe Theophilus", role: "Assistant Organizer", twitter: "Twitter", linkedin: "LinkedIn" },
+    { id: 2, image: '../../../assets/img/bg-yellow.jpeg', name: "Enakhe Theophilus", role: "Assistant Organizer", twitter: "Twitter", linkedin: "LinkedIn" },
     { id: 3, image: '../../../assets/img/team/aremu_david.jpeg', name: "Aremu David", role: "Chapter Mentor", twitter: "https://twitter.com/Khingdave2", linkedin: "https://www.linkedin.com/in/aremu-david-64a63a193" },
     { id: 4, image: '../../../assets/img/team/ajayi_olawale.jpeg', name: "Ajayi Olawale", role: "Chapter Mentor", twitter: "https://twitter.com/Thenameis_Horla", linkedin: "https://www.linkedin.com/in/ajayi-olawale-107674192" },
     { id: 5, image: '../../../assets/img/team/idibia_isaac.jpeg', name: "Idibia Isaac", role: "Chapter Mentor", twitter: "https://twitter.com/IsaacIdibia", linkedin: "https://www.linkedin.com/in/isaac-idibia-105059171" },
